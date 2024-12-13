81ea4663a7f1330f6d148f6fae4fc1ee182d5282
heronh <heronhurpia@gmail.com>
2024-12-12 21:17:50 -0300

    criadas amostras de imagens

--- controllers/adminController.go
+++ controllers/adminController.go
@@ -12,10 +12,19 @@ import (
 
 var dishes []models.Dish = []models.Dish{
 	{Name: "Feijoada", Description: "Feijoada mineira completa", Enabled: true, Price: 25.00},
-	{Name: "Macarronada", Description: "Macarronada com molho de tomate", Enabled: true, Price: 15.00},
-	{Name: "Lasanha", Description: "Lasanha de frango", Enabled: true, Price: 20.00},
-	{Name: "Frango grelhado", Description: "Frango grelhado com batata frita", Enabled: true, Price: 18.00},
+	{Name: "Batata frita", Description: "Deliciosa", Enabled: true, Price: 15.00},
+	{Name: "Batata frita com bacon", Description: "Deliciosa e com bacon crocante", Enabled: true, Price: 15.00},
+	{Name: "Lasanha", Description: "Lasanha a bolonhesa", Enabled: true, Price: 20.00},
+	{Name: "Frango à passarinho", Description: "crocante e com pedaços de alho", Enabled: true, Price: 18.00},
 	{Name: "Bife à parmegiana", Description: "Bife à parmegiana com arroz e batata frita", Enabled: true, Price: 22.00},
+	{Name: "Creme brulee", Description: "Creme brulee com calda de frutas vermelhas", Enabled: true, Price: 10.00},
+	{Name: "Bolo de frutas", Description: "Bolo de frutas com calda de chocolate", Enabled: true, Price: 10.00},
+	{Name: "Sorvete de chocolate", Description: "Sorvete de chocolate com calda de chocolate", Enabled: true, Price: 10.00},
+	{Name: "Coca-cola", Description: "Coca-cola gelada", Enabled: true, Price: 5.00},
+	{Name: "Heineken", Description: "Heineken gelada", Enabled: true, Price: 7.00},
+	{Name: "Suco de laranja", Description: "Suco de laranja natural", Enabled: true, Price: 5.00},
+	{Name: "Moscow mule", Description: "Moscow mule com limão e gengibre", Enabled: true, Price: 15.00},
+	{Name: "Caipirinha", Description: "Caipirinha de limão", Enabled: true, Price: 10.00},
 }
 
 func Admin(c *gin.Context) {
